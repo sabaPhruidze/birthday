7f8053d1d785efb607c8de1a5031c94e1d220815
Saba <phruidzesaba@gmail.com>
2023-06-24 13:41:14 +0400

    I finished the logic part now I have to make the Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { useState, useReducer } from "react";
+import { useState } from "react";
 import "./App.css";
 import { PersonData } from "./components/Persons";
+
 function App() {
+  const [change, setChange] = useState<boolean>(false);
   return (
     <div className="body">
-      <div className="container">
-        <h3>5 birthdays today</h3>
-        <div>
-          {PersonData.map((person: any, idx: number) => (
-            <div className="person-container">
-              <img src={person.img} alt={person.fullname} key={idx} />
-              <div>
-                <p className="info-container">{person.fullname}</p>
-                <p className="info-container">{person.age} years</p>
+      <div className={change ? "container container-plus" : "container"}>
+        <h3>{change ? "0 birthdays today" : "5 birthdays today"}</h3>
+        {change ? (
+          ""
+        ) : (
+          <div>
+            {PersonData.map((person: any, idx: number) => (
+              <div className="person-container" key={idx}>
+                <img src={person.img} alt={person.fullname} />
+                <div>
+                  <p className="info-container">{person.fullname}</p>
+                  <p className="info-container">{person.age} years</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <button>Clear all</button>
+            ))}
+          </div>
+        )}
+        <button onClick={() => setChange(true)}>Clear all</button>
       </div>
     </div>
   );
